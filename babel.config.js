97f6dff4bb8a9e06c08b61d005329c61174160e5
Joshua A. Díaz Robayna <jadrdevcm@gmail.com>
2020-07-31 16:40:59 +0100

    Añadida Configuracion RN Paper
    Añadida Configuracion RN Paper

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  // Añadimos el código para React Native Paper
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
