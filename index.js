5701dd9cbdc2a9f9a619405cfae4f1a6238eb0d0
Joshua A. Díaz Robayna <jadrdevcm@gmail.com>
2020-07-31 16:43:20 +0100

    Importación RNNavigation5
    Importación RNNavigation5

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// Importanción de React Navigation 5
+import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);
